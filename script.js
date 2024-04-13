4234bd4281469fbe12a0b82f2be495744b30ad8c
unknown <typingmaster500@gmail.com>
2024-04-13 21:52:35 +0500

    added

---
+++ script.js
@@ -0,0 +1,7 @@
+let typed=new Typed(".auto_type",{
+    strings:[
+        "Monthly Family Support","Daily Meals", "It and Vocational Training", "clean water", "education" 
+    ],
+    typeSpeed:100,
+    loop:true,
+});
